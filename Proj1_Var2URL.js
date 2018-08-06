d81ce80f4c10f134c4ccfed3da3df49bed150553
Jessica Hudgins <jessicahudgins@Stevens-MacBook-Air-16.local>
2018-08-06 15:18:43 -0400

    Initial Project 1 Commit

---
+++ Proj1_Var2URL.js
@@ -0,0 +1,11 @@
+// Turn a few variables into a string that you will use in a URL.
+
+// make a stringhold it in a variable.
+// fuck with it
+// make it all lower case. 
+
+// append it to your own website or a facebook url 
+
+str = "The St. Petersburg string I\'m going to use to get to DETROIT BABY!"
+
+str.indexOf("St.")
